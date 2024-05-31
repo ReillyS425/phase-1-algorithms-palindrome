3a7df703d272a87fb2bfc6301a13873d8c1282b2
Reilly Shofner <reilly.shofner@gmail.com>
2024-05-30 17:15:32 -0700

    complete algorithms palindrome lab

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  const reversedWord = word.split('').reverse().join('');
+  if (word === reversedWord)
+    { return true
+    }
+       else { return false
+       };
+    }
+      
 
 /* 
-  Add your pseudocode here
+  function isPalindrome(word) {
+    if word equals reversedWord reversed
+    returns word = true if word is same reversed;
+    else word returns false;
+  }
 */
 
 /*
-  Add written explanation of your solution here
+  The isPalindrome funtion takes a word as a parameter and using the variable reveresedWord to reverse the word, the function then checks
+  to see if the the word is equal to itself in reversed form and if it is it returns true if not it returns false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("alfred"));
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
